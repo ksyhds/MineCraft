4288904909dbc8644d7ab0a11dc269425d8a3e46
Moon_Eclipse <>
2017-06-10 17:46:11 +0900

    .

--- NameItemDrop 1.11/src/com/Moon_Eclipse/NameItemDrop/NameItemDrop.java
+++ NameItemDrop 1.11/src/com/Moon_Eclipse/NameItemDrop/NameItemDrop.java
@@ -17,7 +17,10 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Slime;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
@@ -123,57 +126,65 @@ public class NameItemDrop extends JavaPlugin implements Listener
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent e)
 	{
+		//Bukkit.broadcastMessage("이벤트 FIRE - NID");
 		Entity entity = e.getEntity();
-		if(entity instanceof Creature)
+		if(entity instanceof Creature || entity instanceof Slime)
 		{
-		    Creature creature = (Creature) entity;
+			//Bukkit.broadcastMessage("엔티티는 크리쳐에 속함.");
+		    LivingEntity creature = (LivingEntity) entity;
 		    String getname = creature.getCustomName();
 		    if(!((getname + "").equals("null")))
 		    {
+		    	//Bukkit.broadcastMessage("엔티티가 이름을 갖음. 이름: " + getname);
 		    	String name = getname.replace(" ", "_");
 			    String pluginname = name.replace("§", "&");
 			    
 			    List<ItemStack> newdrops = e.getDrops();
 			    //Bukkit.broadcastMessage(pluginname);
-			    String path = "mobs." + pluginname;
-			    int exp = mobs.getInt(path + ".exp");
-			    e.setDroppedExp(exp);
-			    
-		   		if(mobs.contains(path))
-		   		{
-	   				int	chanceint = c.getInt("config.ChanceInt");
-		   			List<String> itemlist = mobs.getStringList(path + ".items");
-		   			for(String item : itemlist)
-		   			{
-		   				// ("1, 10")
-		   				String perstr = item.substring(item.indexOf(",") + 1, item.length());
-		   				double perint = Double.parseDouble(perstr);
-		   				//Bukkit.broadcastMessage(perstr + " + perstr");
-		   				int random = rn.nextInt(chanceint -1) + 1;
-		   				double percent = ((double)random / (double)chanceint) * 100;
-		   				//Bukkit.broadcastMessage(perint + " + perint");
-		   				//Bukkit.broadcastMessage(percent + " + percent");
-		   				if(percent <= perint)
-		   				{
-		   					String itemname = item.substring(0 , item.indexOf(","));
-		   					String target = "items." + itemname;
-		   					//Bukkit.broadcastMessage(itemname + " + itemname");
-		   					if(items.contains(target))
-		   					{
-		   						int itemid = items.getInt(target + ".id");
-				   				int amount = items.getInt(target + ".amount");
-				   				int metadata = items.getInt(target + ".metadata");
-				   				String color = items.getString(target + ".color");
-				   				String Disname = items.getString(target + ".name");
-				   				List<String> lore = items.getStringList(target + ".lore");
-				   				List<String> enchant = items.getStringList(target + ".enchants");
-				   				ItemStack dropitem = this.createItem(itemid, amount, Disname, lore, color, enchant, metadata);
-				   				dropitem = LibMain.hideFlags_Unbreak(dropitem);
-				   				newdrops.add(dropitem);
-		   					}
-		   				}
-		   			}
-		   		}
+			    Set<String> keys = mobs.getConfigurationSection("mobs").getKeys(false);
+			    for(String key : keys)
+			    {
+			    	if(pluginname.contains(key))
+			   		{
+			    		//Bukkit.broadcastMessage("이름을 콘픽에서 찾음. 콘픽 이름: " + key);
+			   			String path = "mobs." + key;
+			   			int exp = mobs.getInt(path + ".exp");
+					    e.setDroppedExp(exp);
+		   				int	chanceint = c.getInt("config.ChanceInt");
+			   			List<String> itemlist = mobs.getStringList(path + ".items");
+			   			for(String item : itemlist)
+			   			{
+			   				// ("1, 10")
+			   				String perstr = item.substring(item.indexOf(",") + 1, item.length());
+			   				double perint = Double.parseDouble(perstr);
+			   				//Bukkit.broadcastMessage(perstr + " + perstr");
+			   				int random = rn.nextInt(chanceint -1) + 1;
+			   				double percent = ((double)random / (double)chanceint) * 100;
+			   				//Bukkit.broadcastMessage(perint + " + perint");
+			   				//Bukkit.broadcastMessage(percent + " + percent");
+			   				if(percent <= perint)
+			   				{
+			   					String itemname = item.substring(0 , item.indexOf(","));
+			   					String target = "items." + itemname;
+			   					//Bukkit.broadcastMessage(itemname + " + itemname");
+			   					if(items.contains(target))
+			   					{
+			   						int itemid = items.getInt(target + ".id");
+					   				int amount = items.getInt(target + ".amount");
+					   				int metadata = items.getInt(target + ".metadata");
+					   				String color = items.getString(target + ".color");
+					   				String Disname = items.getString(target + ".name");
+					   				List<String> lore = items.getStringList(target + ".lore");
+					   				List<String> enchant = items.getStringList(target + ".enchants");
+					   				ItemStack dropitem = this.createItem(itemid, amount, Disname, lore, color, enchant, metadata);
+					   				dropitem = LibMain.hideFlags_Unbreak(dropitem);
+					   				newdrops.add(dropitem);
+			   					}
+			   				}
+			   			}
+			   			break;
+			   		}
+			    }
 		    }
 		}
 	}
